ae95f5f08530a1e5ffbc3b1dadee4aa561dc3e7a
Karolis <karolis.lipi@gmail.com>
2025-12-27 04:27:04 +0200

    Adding Initial UI layout (Tabs + Image View)

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,11 +1,30 @@
+using System.Collections.ObjectModel;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace ArchiveApp;
 
 public partial class MainWindow : Window
 {
+    public ObservableCollection<string> Tabs { get; } = ["Tab1"];
+    private int _counter = 2;
+    
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = this;
+    }
+
+    private void NewTabButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Tabs.Add($"Tab{_counter++}");
+    }
+
+    private void CloseTabButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Button { DataContext: string tab })
+        {
+            Tabs.Remove(tab);
+        }
     }
 }
---
+++ views/GalleryView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ArchiveApp.views;
+
+public partial class GalleryView : UserControl
+{
+    public GalleryView()
+    {
+        InitializeComponent();
+    }
+}
